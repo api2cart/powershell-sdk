29f8e54218bba5467f4f219eabad6dd092acb474
GitHub Action <action@github.com>
2025-08-24 01:22:08 +0000

    Update powershell SDK [skip ci]

--- Build.ps1
+++ Build.ps1
@@ -60,7 +60,7 @@ $Manifest = @{
     ModuleVersion = '0.1.2'
 
     RootModule = 'PSOpenAPITools.psm1'
-    Guid = '{ABA5A234-59EC-43C7-BA5B-748F143F2616}' # Has to be static, otherwise each new build will be considered different module
+    Guid = '{9D96C647-1751-4382-8E9E-DEF3C90883DB}' # Has to be static, otherwise each new build will be considered different module
 
     PowerShellVersion = '6.2'
 
